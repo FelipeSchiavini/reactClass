e727ccfa1c03136024f6f503ba26e1abaef89e41
FelipeSchiavini <52425298+FelipeSchiavini@users.noreply.github.com>
2021-08-09 21:57:55 -0300

    teste

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,18 +3,31 @@ import { useState } from 'react';
 
 function Home() {
   const [texto, setTexto] = useState('');
+  const [texto2, setTexto2] = useState('');
 
   const atualizarTexto = (evento) => {
     setTexto(evento.target.value);
   };
 
+  const atualizahtml = (evento) => {
+    setTexto2(texto)
+  }
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Escreva seu texto"
-        onChange={atualizarTexto} />
-      <p className="citacao">{texto}</p>
+      <div>
+        <blockquote>
+          {texto2}
+        </blockquote>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Escreva seu texto"
+          onChange={atualizarTexto} />
+        <button
+          onClick = {atualizahtml}>Inserir Citação</button>
+      </div>
       <Footer paginaAtual='Contato' />
     </div>
   );
